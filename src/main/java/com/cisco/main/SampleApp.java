148849a5c3a1735ceae07a6c5aeecfc5227da642
vsubramanian <vsubramanian@mobitv.com>
2013-06-27 14:51:43 +0530

    ss

---
+++ src/main/java/com/cisco/main/SampleApp.java
@@ -0,0 +1,15 @@
+package com.cisco.main;
+
+public class SampleApp {
+
+	public static void main(String args[])
+	{
+		
+	}
+	
+	public void test()
+	{
+		
+	}
+	
+}
---
+++ src/test/java/CiscoTest.java
@@ -0,0 +1,20 @@
+
+import java.io.File;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class CiscoTest extends TestCase {
+    
+    
+    public void setUp() {
+       
+ 
+	}
+    
+    public void testCisco() {
+        String path = null;
+        assertEquals("passed", "passed");
+    }
+  
+}
